a25e7a170803bb7f9b984a2b89d9376b32386cd0
Chris Knepper <chris82thekid@gmail.com>
2018-07-27 21:25:11 -0400

    -Implement spell-checking and context menu suggestions via electron-spell-check-provider

--- src/helpers/webview/bridge.js
+++ src/helpers/webview/bridge.js
@@ -1,12 +1,62 @@
 // This script is injected into the webview.
 // Newer ES6 features (import/export syntax etc...) are not allowed here nor in any JS which this imports.
 
-const popupContextMenu = require('./context_menu.js');
+//const popupContextMenu = require('./context_menu.js');
 const { EVENT_WEBVIEW_NOTIFICATION, EVENT_NOTIFICATION_REFLECT_READY } = require('../../constants');
 const { ipcRenderer, remote } = require('electron')
 
+var webFrame = require('electron').webFrame;
+var SpellCheckProvider = require('electron-spell-check-provider');
+// `remote.require` since `Menu` is a main-process module.
+var buildEditorContextMenu = remote.require('electron-editor-context-menu');
+
+var selection;
+function resetSelection() {
+    selection = {
+        isMisspelled: false,
+        spellingSuggestions: []
+    };
+}
+resetSelection();
+
+// Reset the selection when clicking around, before the spell-checker runs and the context menu shows.
+window.addEventListener('mousedown', resetSelection);
+
+// The spell-checker runs when the user clicks on text and before the 'contextmenu' event fires.
+// Thus, we may retrieve spell-checking suggestions to put in the menu just before it shows.
+webFrame.setSpellCheckProvider(
+    'en-US',
+    // Not sure what this parameter (`autoCorrectWord`) does: https://github.com/atom/electron/issues/4371
+    // The documentation for `webFrame.setSpellCheckProvider` passes `true` so we do too.
+    true,
+    new SpellCheckProvider('en-US').on('misspelling', function (suggestions) {
+        // Prime the context menu with spelling suggestions _if_ the user has selected text. Electron
+        // may sometimes re-run the spell-check provider for an outdated selection e.g. if the user
+        // right-clicks some misspelled text and then an image.
+        if (window.getSelection().toString()) {
+            selection.isMisspelled = true;
+            // Take the first three suggestions if any.
+            selection.spellingSuggestions = suggestions.slice(0, 3);
+        }
+    }));
+
+window.addEventListener('contextmenu', function (e) {
+    // Only show the context menu in text editors.
+    if (!e.target.closest('textarea, input, [contenteditable="true"]')) return;
+
+    console.log('we got a thing boss', selection);
+    var menu = buildEditorContextMenu(selection);
+
+    // The 'contextmenu' event is emitted after 'selectionchange' has fired but possibly before the
+    // visible selection has changed. Try to wait to show the menu until after that, otherwise the
+    // visible selection will update after the menu dismisses and look weird.
+    setTimeout(function () {
+        menu.popup(remote.getCurrentWindow());
+    }, 30);
+});
+
 // Electron (or the build of Chromium it uses?) does not seem to have any default right-click menu, this adds our own.
-window.addEventListener('contextmenu', popupContextMenu);
+//window.addEventListener('contextmenu', popupContextMenu);
 
 const OriginalBrowserNotification = Notification;
 
