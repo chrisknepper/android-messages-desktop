cc086929aca1ac96077eb3577cf4f4d6b02b2634
Andrew Jivoin <andrew@jivoin.com>
2018-09-05 17:14:18 -0400

    Fixes placement of check for update button on windows.

--- src/menu/file_menu_template.js
+++ src/menu/file_menu_template.js
@@ -1,16 +1,19 @@
 import { app } from 'electron';
+import { IS_WINDOWS } from '../constants';
 import { checkForUpdatesMenuItem } from './items/check_for_updates';
+import { separator } from './items/separator';
+
+const submenu = [{
+    label: 'Quit Android Messages',
+    click: () => app.quit()
+}];
+
+if (!IS_WINDOWS) {
+    submenu.unshift(separator);
+    submenu.unshift(checkForUpdatesMenuItem);
+}
 
 export const fileMenuTemplate = {
     label: 'File',
-    submenu: [
-        checkForUpdatesMenuItem,
-        {
-            type: 'separator'
-        },
-        {
-            label: 'Quit Android Messages',
-            click: () => app.quit()
-        }
-    ]
+    submenu
 };
