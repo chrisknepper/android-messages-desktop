3fc27490705dede7582ed8645963d2ff3c96bd74
Chris Knepper <chris82thekid@gmail.com>
2019-02-04 21:34:21 -0500

    -Update copyright year -Add Google disclaimer in about window -Be more specific about electron-hunspell version since it is updated incredibly frequently

--- src/menu/items/about.js
+++ src/menu/items/about.js
@@ -6,10 +6,11 @@ import { description } from '../../../package.json';
 
 let languageCode = '';
 let descriptionWithLocale = '';
+const localeStyle = '-webkit-app-region: no-drag; position: absolute; left: 0.5em; bottom: 0.5em; font-size: 12px; color: #999';
+const disclaimerText = '<br><br>Not affiliated with Google in any way.<br>Android is a trademark of Google LLC.'; 
 app.on('ready', () => {
     languageCode = app.getLocale();
     // about-window does not have a field for arbitrary HTML, so we add the HTML we need to an existing field
-    const localeStyle = '-webkit-app-region: no-drag; position: absolute; left: 0.5em; bottom: 0.5em; font-size: 12px; color: #999';
     descriptionWithLocale = `${description}<span style="${localeStyle}">${languageCode}</span>`;
 });
 
@@ -18,11 +19,12 @@ export const aboutMenuItem = {
     click: () => {
         openAboutWindow({
             icon_path: appIcon,
-            copyright: 'Copyright © 2018 Chris Knepper, All rights reserved.',
+            copyright: `<div style="text-align: center">Copyright © 2018-2019 Chris Knepper, All rights reserved.${disclaimerText}</div>`,
             product_name: 'Android Messages Desktop',
             description: descriptionWithLocale,
             open_devtools: IS_DEV,
-            use_inner_html: true
+            use_inner_html: true,
+            adjust_window_size: true
         });
     }
 };
