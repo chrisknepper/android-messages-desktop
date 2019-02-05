71b89733b9fe416e0beaf8b88ac9076dc615fcc8
Chris Knepper <chris82thekid@gmail.com>
2019-02-04 20:39:07 -0500

    -Display locale code in about window

--- src/menu/items/about.js
+++ src/menu/items/about.js
@@ -1,6 +1,17 @@
 import appIcon from '../../../resources/icons/512x512.png';
 import { IS_DEV } from '../../constants';
 import openAboutWindow from 'about-window';
+import { app } from 'electron';
+import { description } from '../../../package.json';
+
+let languageCode = '';
+let descriptionWithLocale = '';
+app.on('ready', () => {
+    languageCode = app.getLocale();
+    // about-window does not have a field for arbitrary HTML, so we add the HTML we need to an existing field
+    const localeStyle = '-webkit-app-region: no-drag; position: absolute; left: 0.5em; bottom: 0.5em; font-size: 12px; color: #999';
+    descriptionWithLocale = `${description}<span style="${localeStyle}">${languageCode}</span>`;
+});
 
 export const aboutMenuItem = {
     label: 'About Android Messages Desktop',
@@ -9,7 +20,9 @@ export const aboutMenuItem = {
             icon_path: appIcon,
             copyright: 'Copyright © 2018 Chris Knepper, All rights reserved.',
             product_name: 'Android Messages Desktop',
-            open_devtools: IS_DEV
+            description: descriptionWithLocale,
+            open_devtools: IS_DEV,
+            use_inner_html: true
         });
     }
 };
