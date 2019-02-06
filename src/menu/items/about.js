186b534ce2da0856a64ff217dc17ce58f3fb6b4e
Chris Knepper <chris82thekid@gmail.com>
2019-02-05 20:14:13 -0500

    -About window additional text for licenses and copy -Delay about window until it's ready to show

--- src/menu/items/about.js
+++ src/menu/items/about.js
@@ -4,10 +4,14 @@ import openAboutWindow from 'about-window';
 import { app } from 'electron';
 import { description } from '../../../package.json';
 
+const productName = 'Android Messages Desktop';
+const localeStyle = '-webkit-app-region: no-drag; position: absolute; left: 0.5em; bottom: 0.5em; font-size: 12px; color: #999';
+const disclaimerText = '<br><br>Not affiliated with Google in any way.<br>Android is a trademark of Google LLC.';
+const licenseText = `<br><br>${productName} is released under the MIT License.`;
+const dictionaryLicenseText = `<br><br>Spelling dictionaries are released under various licenses including MIT, BSD, and GNU GPL. <a class="link" href="https://github.com/wooorm/dictionaries#table-of-dictionaries" style="text-decoration: none">See dictionary license details</a>.`
+
 let languageCode = '';
 let descriptionWithLocale = '';
-const localeStyle = '-webkit-app-region: no-drag; position: absolute; left: 0.5em; bottom: 0.5em; font-size: 12px; color: #999';
-const disclaimerText = '<br><br>Not affiliated with Google in any way.<br>Android is a trademark of Google LLC.'; 
 app.on('ready', () => {
     languageCode = app.getLocale();
     // about-window does not have a field for arbitrary HTML, so we add the HTML we need to an existing field
@@ -15,16 +19,22 @@ app.on('ready', () => {
 });
 
 export const aboutMenuItem = {
-    label: 'About Android Messages Desktop',
+    label: `About ${productName}`,
     click: () => {
         openAboutWindow({
             icon_path: appIcon,
-            copyright: `<div style="text-align: center">Copyright © 2018-2019 Chris Knepper, All rights reserved.${disclaimerText}</div>`,
-            product_name: 'Android Messages Desktop',
+            copyright: `<div style="text-align: center">Copyright © 2018-2019 Chris Knepper, All rights reserved.${disclaimerText}${licenseText}${dictionaryLicenseText}</div>`,
+            product_name: productName,
             description: descriptionWithLocale,
             open_devtools: IS_DEV,
             use_inner_html: true,
-            adjust_window_size: true
+            win_options: {
+                height: 500,
+                resizable: false,
+                minimizable: false,
+                maximizable: false,
+                show: false // Delays showing until content is ready, prevents FOUC/flash of blank white window
+            }
         });
     }
 };
