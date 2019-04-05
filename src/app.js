120f672daa704e455c6c85d625d7f75f43d3c2c6
Chris Knepper <chris82thekid@gmail.com>
2019-04-05 18:18:56 -0400

    -Make presence and absence of slashes consistent with old android URL

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ androidMessagesWebview.addEventListener('did-start-loading', () => {
   androidMessagesWebview.getWebContents().session.webRequest.onHeadersReceived({
     // Only run this code on requests for which the URL is in the following array.
     // The SRC of the webview is the same context as the preload script.
-    urls: ['https://messages.google.com/web'] }, (details, callback) => {
+    urls: ['https://messages.google.com/web/'] }, (details, callback) => {
       /*
        * Google sends several directives in the content-security-policy header which restrict what kind of JS can run and where it can originate.
        * This will break our spell checking (because it instantiates a WebAssembly module) unless we include unsafe-eval for the root page.
