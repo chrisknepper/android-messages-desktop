870f8f393fe57c8b759563a410a27d053dc8c867
Kyle Rosenberg <kyle@ekrosenberg.com>
2021-02-20 16:22:03 -0500

    catch errors from checking for updates and only install update if in prod

--- src/helpers/autoUpdate.ts
+++ src/helpers/autoUpdate.ts
@@ -1,7 +1,7 @@
 import { autoUpdater } from "electron-updater";
 import { app, Notification } from "electron";
 import path from "path";
-import { RESOURCES_PATH } from "./constants";
+import { IS_DEV, RESOURCES_PATH } from "./constants";
 
 function setUpdaterSettings(): void {
   autoUpdater.autoDownload = false;
@@ -15,28 +15,33 @@ export async function checkForUpdate(
   showNotification: boolean
 ): Promise<boolean> {
   setUpdaterSettings();
-  const results = await autoUpdater.checkForUpdates();
-  const isUpdate = results.updateInfo.version != app.getVersion();
-  if (isUpdate && showNotification) {
-    const notification = new Notification({
-      title: "Update Available",
-      body:
-        'There is an update available. Click "Install Update"' +
-        " in the file or app menu.",
-      icon: path.resolve(RESOURCES_PATH, "icons", "64x64.png"),
-    });
-    notification.show();
+  const results = await autoUpdater.checkForUpdates().catch(() => null);
+  if (results != null) {
+    const isUpdate = results.updateInfo.version != app.getVersion();
+    if (isUpdate && showNotification) {
+      const notification = new Notification({
+        title: "Update Available",
+        body:
+          'There is an update available. Click "Install Update"' +
+          " in the file or app menu.",
+        icon: path.resolve(RESOURCES_PATH, "icons", "64x64.png"),
+      });
+      notification.show();
+    }
+    return isUpdate;
   }
-  return isUpdate;
+  return false;
 }
 
 /**
  * Checks for update, downloads, quits, and installs app.
  */
 export async function installUpdate(): Promise<void> {
-  setUpdaterSettings();
-  if (await checkForUpdate(false)) {
-    await autoUpdater.downloadUpdate();
-    autoUpdater.quitAndInstall();
+  if (!IS_DEV) {
+    setUpdaterSettings();
+    if (await checkForUpdate(false)) {
+      await autoUpdater.downloadUpdate();
+      autoUpdater.quitAndInstall();
+    }
   }
 }
