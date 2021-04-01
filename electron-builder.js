404cd9ef79efba772574bc9a46469ea580c9baeb
Restyled.io <commits@restyled.io>
2021-04-01 01:07:15 +0000

    Restyled by prettier

--- electron-builder.js
+++ electron-builder.js
@@ -1,33 +1,33 @@
 module.exports = {
-  appId : "pw.electron.android-messages",
-  artifactName : "${productName}-v${version}-${os}-${arch}.${ext}",
-  productName : "AndroidMessages",
-  copyright : "Copyright 2020 Kyle Rosenberg",
-  files : [ "app/**/*", "resources/**/*" ],
-  directories : {
-    buildResources : "resources",
-    output : "dist",
+  appId: "pw.electron.android-messages",
+  artifactName: "${productName}-v${version}-${os}-${arch}.${ext}",
+  productName: "AndroidMessages",
+  copyright: "Copyright 2020 Kyle Rosenberg",
+  files: ["app/**/*", "resources/**/*"],
+  directories: {
+    buildResources: "resources",
+    output: "dist",
   },
-  linux : {
-    target : [ "AppImage", "snap", "deb", "pacman", "rpm", "freebsd", "zip" ],
-    executableName : "AndroidMessages",
-    category : "Internet",
-    desktop : {
-      StartupWMClass : "android-messages-desktop",
+  linux: {
+    target: ["AppImage", "snap", "deb", "pacman", "rpm", "freebsd", "zip"],
+    executableName: "AndroidMessages",
+    category: "Internet",
+    desktop: {
+      StartupWMClass: "android-messages-desktop",
     },
   },
-  win : {
-    target : [ "nsis", "portable" ],
+  win: {
+    target: ["nsis", "portable"],
   },
-  mac : {
-    category : "public.app-category.social-networking",
-    target : {target : "default", arch : "universal"}
+  mac: {
+    category: "public.app-category.social-networking",
+    target: { target: "default", arch: "universal" },
   },
-  portable : {
-    artifactName : "${productName}-v${version}-${os}-${arch}.portable.${ext}",
+  portable: {
+    artifactName: "${productName}-v${version}-${os}-${arch}.portable.${ext}",
   },
-  nsis : {
-    allowToChangeInstallationDirectory : true,
-    oneClick : false,
+  nsis: {
+    allowToChangeInstallationDirectory: true,
+    oneClick: false,
   },
 };
