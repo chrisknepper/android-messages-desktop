5f1d9f1b48cd1f9c8205257fb65e68f1c6b8615e
Kyle Rosenberg <kyle@ekrosenberg.com>
2021-12-19 13:35:39 -0500

    small cleanup of various things

--- src/background.ts
+++ src/background.ts
@@ -2,7 +2,6 @@ import { app, Event as ElectronEvent, Menu, shell } from "electron";
 import path from "path";
 import { checkForUpdate } from "./helpers/autoUpdate";
 import {
-  BASE_APP_PATH,
   IS_DEV,
   IS_LINUX,
   IS_MAC,
@@ -27,162 +26,150 @@ const {
 } = settings;
 
 let mainWindow: CustomBrowserWindow;
+let trayManager: TrayManager;
+
+app.on("second-instance", () => {
+  if (mainWindow) {
+    if (!mainWindow.isVisible()) {
+      mainWindow.show();
+    }
+  }
+});
+
+if (!app.requestSingleInstanceLock()) {
+  app.quit();
+}
+
+const setApplicationMenu = () => {
+  const menus = baseMenuTemplate;
+  if (IS_DEV) {
+    menus.push(devMenuTemplate);
+  }
+  menus.push(helpMenuTemplate);
+  Menu.setApplicationMenu(Menu.buildFromTemplate(menus));
+};
 
 /**
- * Prevent multiple instances of the app which causes many problems with an app like ours
- * Without this, if an instance were minimized to the tray in Windows, clicking a shortcut would launch another instance, icky
- * Adapted from https://www.electronjs.org/docs/api/app#apprequestsingleinstancelock
+ * Save userData in separate folders for each environment.
+ * Thanks to this you can use production and development versions of the app
+ * on same machine like those are two separate apps.
  */
-const isFirstInstance = app.requestSingleInstanceLock();
+if (IS_DEV) {
+  const userDataPath = app.getPath("userData");
+  app.setPath("userData", `${userDataPath}-(${process.env.NODE_ENV})`);
+}
 
-if (!isFirstInstance) {
-  app.quit();
-} else {
-  app.on("second-instance", () => {
-    if (mainWindow) {
-      if (!mainWindow.isVisible()) {
-        mainWindow.show();
+if (IS_WINDOWS) {
+  app.setAppUserModelId("pw.kmr.android-messages-desktop");
+  app.setAsDefaultProtocolClient("android-messages-desktop");
+}
+
+app.on("ready", () => {
+  trayManager = new TrayManager();
+
+  setApplicationMenu();
+
+  if (IS_MAC) {
+    app.on("activate", () => {
+      mainWindow.show();
+    });
+  }
+
+  if (checkForUpdateOnLaunchEnabled.value && !IS_DEV) {
+    checkForUpdate(true);
+  }
+
+  const { width, height } = savedWindowSize.value;
+  const { x, y } = savedWindowPosition.value ?? {};
+
+  mainWindow = new CustomBrowserWindow("main", {
+    width,
+    height,
+    x,
+    y,
+    autoHideMenuBar: autoHideMenuEnabled.value,
+    show: false, //don't show window just yet (issue #229)
+    icon: IS_LINUX
+      ? path.resolve(RESOURCES_PATH, "icons", "128x128.png")
+      : undefined,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+      preload: path.resolve(app.getAppPath(), "bridge.js"),
+    },
+  });
+
+  // set user agent to potentially make google fi work
+  const userAgent =
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0";
+
+  mainWindow.webContents.session.webRequest.onBeforeSendHeaders(
+    {
+      urls: ["https://accounts.google.com/*"],
+    },
+    ({ requestHeaders }, callback) =>
+      callback({
+        requestHeaders: { ...requestHeaders, "User-Agent": userAgent },
+      })
+  );
+
+  // Quick and dirty way for renderer process to access mainWindow for communication
+  app.mainWindow = mainWindow;
+  app.trayManager = trayManager;
+  app.settings = settings;
+
+  mainWindow.loadURL("https://messages.google.com/web/");
+
+  trayManager.startIfEnabled();
+
+  let quitViaContext = false;
+  app.on("before-quit", () => {
+    quitViaContext = true;
+  });
+
+  const shouldExitOnMainWindowClosed = () => {
+    if (IS_MAC) {
+      return quitViaContext;
+    } else {
+      if (trayEnabled.value) {
+        return quitViaContext;
       }
+      return true;
+    }
+  };
+
+  mainWindow.on("close", (event: ElectronEvent) => {
+    const { x, y, width, height } = mainWindow.getBounds();
+    savedWindowPosition.next({ x, y });
+    savedWindowSize.next({ width, height });
+    if (!shouldExitOnMainWindowClosed()) {
+      event.preventDefault();
+      mainWindow.hide();
+      trayManager?.showMinimizeToTrayWarning();
+    } else {
+      app.quit(); // If we don't explicitly call this, the webview and mainWindow get destroyed but background process still runs.
     }
   });
 
-  const setApplicationMenu = () => {
-    const menus = baseMenuTemplate;
-    if (IS_DEV) {
-      menus.push(devMenuTemplate);
+  mainWindow.webContents.on("new-window", (e, url) => {
+    e.preventDefault();
+    shell.openExternal(url);
+  });
+
+  mainWindow.webContents.on("context-menu", popupContextMenu);
+
+  // block Google collecting data
+  mainWindow.webContents.session.webRequest.onBeforeRequest(
+    {
+      urls: [
+        "https://messages.google.com/web/jserror?*",
+        "https://play.google.com/log?*",
+        "https://www.google-analytics.com/analytics.js",
+      ],
+    },
+    (details, callback) => {
+      callback({ cancel: true });
     }
-    menus.push(helpMenuTemplate);
-    Menu.setApplicationMenu(Menu.buildFromTemplate(menus));
-  };
-
-  /**
-   * Save userData in separate folders for each environment.
-   * Thanks to this you can use production and development versions of the app
-   * on same machine like those are two separate apps.
-   */
-  if (IS_DEV) {
-    const userDataPath = app.getPath("userData");
-    app.setPath("userData", `${userDataPath}-(${process.env.NODE_ENV})`);
-  }
-
-  if (IS_WINDOWS) {
-    // Stupid, DUMB calls that have to be made to let notifications come through on Windows (only Windows 10?)
-    // See: https://github.com/electron/electron/issues/10864#issuecomment-382519150
-    app.setAppUserModelId("pw.kmr.android-messages-desktop");
-    app.setAsDefaultProtocolClient("android-messages-desktop");
-  }
-
-  let trayManager: TrayManager;
-
-  app.on("ready", () => {
-    trayManager = new TrayManager();
-
-    setApplicationMenu();
-
-    if (IS_MAC) {
-      app.on("activate", () => {
-        mainWindow.show();
-      });
-    }
-
-    if (checkForUpdateOnLaunchEnabled.value && !IS_DEV) {
-      checkForUpdate(true);
-    }
-
-    // destructure from the settings
-    const { width, height } = savedWindowSize.value;
-    // provide empty object if savedWindowPosition is null
-    const { x, y } = savedWindowPosition.value || {};
-
-    mainWindow = new CustomBrowserWindow("main", {
-      width,
-      height,
-      x,
-      y,
-      autoHideMenuBar: autoHideMenuEnabled.value,
-      show: false, //don't show window just yet (issue #229)
-      icon: IS_LINUX
-        ? path.resolve(RESOURCES_PATH, "icons", "128x128.png")
-        : undefined,
-      webPreferences: {
-        nodeIntegration: true,
-        contextIsolation: false,
-        enableRemoteModule: true,
-        preload: path.resolve(app.getAppPath(), "bridge.js"),
-      },
-    });
-
-    // set user agent to potentially make google fi work
-    const userAgent =
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0";
-
-    mainWindow.webContents.session.webRequest.onBeforeSendHeaders(
-      {
-        urls: ["https://accounts.google.com/*"],
-      },
-      ({ requestHeaders }, callback) =>
-        callback({
-          requestHeaders: { ...requestHeaders, "User-Agent": userAgent },
-        })
-    );
-
-    // Quick and dirty way for renderer process to access mainWindow for communication
-    app.mainWindow = mainWindow;
-    app.trayManager = trayManager;
-    app.settings = settings;
-
-    mainWindow.loadURL("https://messages.google.com/web/");
-
-    trayManager.startIfEnabled();
-
-    let quitViaContext = false;
-    app.on("before-quit", () => {
-      quitViaContext = true;
-    });
-
-    const shouldExitOnMainWindowClosed = () => {
-      if (IS_MAC) {
-        return quitViaContext;
-      } else {
-        if (trayEnabled.value) {
-          return quitViaContext;
-        }
-        return true;
-      }
-    };
-
-    mainWindow.on("close", (event: ElectronEvent) => {
-      const { x, y, width, height } = mainWindow.getBounds();
-      savedWindowPosition.next({ x, y });
-      savedWindowSize.next({ width, height });
-      if (!shouldExitOnMainWindowClosed()) {
-        event.preventDefault();
-        mainWindow.hide();
-        trayManager?.showMinimizeToTrayWarning();
-      } else {
-        app.quit(); // If we don't explicitly call this, the webview and mainWindow get destroyed but background process still runs.
-      }
-    });
-
-    mainWindow.webContents.on("new-window", (e, url) => {
-      e.preventDefault();
-      shell.openExternal(url);
-    });
-
-    mainWindow.webContents.on("context-menu", popupContextMenu);
-
-    // block Google collecting data
-    mainWindow.webContents.session.webRequest.onBeforeRequest(
-      {
-        urls: [
-          "https://messages.google.com/web/jserror?*",
-          "https://play.google.com/log?*",
-          "https://www.google-analytics.com/analytics.js",
-        ],
-      },
-      (details, callback) => {
-        callback({ cancel: true });
-      }
-    );
-  }); //onready
-}
+  );
+}); //onready
