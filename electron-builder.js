41fcf15d167e45cf3a4a8bbc654508f05c88344b
Kyle Rosenberg <kyle@ekrosenberg.com>
2022-04-23 15:05:59 -0400

    add space to produc name

--- electron-builder.js
+++ electron-builder.js
@@ -1,7 +1,7 @@
 module.exports = {
   appId: "pw.kmr.amd",
   artifactName: "${productName}-v${version}-${os}-${arch}.${ext}",
-  productName: "AndroidMessages",
+  productName: "Android Messages",
   copyright: "Copyright 2020 Kyle Rosenberg",
   files: ["app/**/*", "resources/**/*"],
   directories: {
