505fb523dbb9ccdbb2e91f6ef3395e31110b0b4d
Kyle Rosenberg <kyle@ekrosenberg.com>
2022-09-14 18:16:35 -0400

    bump user agent for google fi (#395)

--- src/background.ts
+++ src/background.ts
@@ -89,7 +89,7 @@ app.on("ready", () => {
   }
   // set user agent to potentially make google fi work
   const userAgent =
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0";
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) Gecko/20100101 Firefox/104.0";
 
   mainWindow.webContents.session.webRequest.onBeforeSendHeaders(
     {
